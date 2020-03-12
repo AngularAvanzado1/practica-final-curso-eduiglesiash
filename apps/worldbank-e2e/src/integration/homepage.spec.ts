597a8b27e073de95600039f836512f3b2b051d86
Edu Iglesias <eiglesiash@gmail.com>
2020-03-12 13:21:55 +0100

    Test Con cypres done

---
+++ apps/worldbank-e2e/src/integration/homepage.spec.ts
@@ -0,0 +1,39 @@
+import { getHeadingH1, getHeadingH2, getButton } from '../support/app.po';
+
+describe('worldbank web app', () => {
+  context('When user visit HomePage', () => {
+    beforeEach(() => cy.visit('/'));
+    it(`should display <h1> with a text "World Bank Application"`, () => {
+      // Custom command example, see `../support/commands.ts` file
+      // cy.login('my-email@something.com', 'myPassword');
+      // Function helper example, see `../support/app.po.ts` file
+      getHeadingH1().contains('World Bank Application');
+    });
+
+    it(`should display <h2> with a text "Continental Region"`, () => {
+      getHeadingH2().contains('Continental Regions');
+    });
+
+    it('clicks the link "Europa y Asia Central"', ()=>{
+      cy.contains(' Europa y Asia central').click();
+    })
+  });
+  context('When user visit Region', ()=>{
+    it(`should display <h2> with a text "Region"`, () => {
+      getHeadingH2().contains('Region');
+    });
+    it('clicks the link "España"', ()=>{
+      cy.contains(' España').click();
+    })
+  });
+  context('When user visit Country', ()=> {
+    it(`should display <h2> with a text "Country"`, ()=>{
+      getHeadingH2().contains('Country: ');
+    });
+
+    it('clicks the button "Region" display content', ()=> {
+      getButton().contains('Region').click();
+    })
+
+  })
+});
--- apps/worldbank-e2e/src/support/app.po.ts
+++ apps/worldbank-e2e/src/support/app.po.ts
@@ -1,1 +1,3 @@
-export const getGreeting = () => cy.get('h1');
+export const getHeadingH1 = () => cy.get('h1');
+export const getHeadingH2 = () => cy.get('h2');
+export const getButton = () => cy.get('button');
